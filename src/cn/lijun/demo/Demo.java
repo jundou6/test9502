05547c392de0680b1c31d035ceb4bd46cc24f48e
jundou6 <15138956487@163.com>
2019-09-04 15:56:19 +0800

    第二个版本

---
+++ src/cn/lijun/demo/Demo.java
@@ -0,0 +1,11 @@
+package cn.lijun.demo;
+
+/**
+ * @author lijun
+ * @date 2019/9/4 15:54
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("我的第二个");
+    }
+}
